6cc3240e0cd20ce25665b6868df33bd24c15da3a
小林勇太崇 <kobayashiyut@pivot.jp>
2025-09-30 11:17:28 +0900

    Buttonにmapメソッドを使用し、ansをnullとnullでない場合に分岐

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [right, setRight] = useState(0);
   const [ans, setAns] = useState(null);
   const digits = [0,1,2,3,4,5,6,7,8,9];
+  const operations = ["+","-","*","/"];
+  const sampleDigits = ["apple","banana",2,8,9];
 
   const keyPressed = (key) => {
     console.log("！！！！！");
@@ -23,46 +25,57 @@ function App() {
   }
 
   function equalKeyPressed(){
-    if(ope === "+"){
-      setAns(left+right);
-    }else if(ope === "-"){
-      setAns(left-right);
-    }else if(ope === "*"){
-      setAns(left*right);
-    }else if(ope === "/"){
-      setAns(left/right);
+    if(ans === null){
+      if(ope === "+"){
+        setAns(left+right);
+      }else if(ope === "-"){
+        setAns(left-right);
+      }else if(ope === "*"){
+        setAns(left*right);
+      }else if(ope === "/"){
+        setAns(left/right);
+      }
+    } else {
+      setAns(null);
+      setOpe(null);
+      setLeft(0);
+      setRight(0);
     }
   }
 
   return (
     <div className="calc">
-      <header>電卓</header>
+      <header>culcs</header>
       <div className="display">
         {left}
         {ope === null ? "" : ope}
         {ope === null ? "" : right}
-        { ans === null ? "" : " 答え:"+ans }
+        {ans === null ? "" : " answer:"+ans }
       </div>
 
       <div className="ButtonWrap">
         <div className="ButtonWrap_left">
 
-        {digits.map((n) => (
-          console.log(n),
+        {digits.map((n , i) => (
           <Button
           key={n}
           text={String(n)}
           onClick={() => keyPressed(n)}
+          disabled={ans !== null}
           />
         ))}
 
         <Button text={ans === null ? "=" : "C"} onClick={ ()=> {equalKeyPressed()} } />
-        </div>
-        <div className="ButtonWrap_right">
-          <Button text="+" onClick={ ()=> {opeKeyPressed("+")} } />
-          <Button text="-" onClick={ ()=> {opeKeyPressed("-")} } />
-          <Button text="*" onClick={ ()=> {opeKeyPressed("*")} } />
-          <Button text="/" onClick={ ()=> {opeKeyPressed("/")} } />
+      </div>
+      <div className="ButtonWrap_right">
+        {operations.map((n , i) => (
+          <Button
+          key={n}
+          text={String(n)}
+          onClick={() => opeKeyPressed(n)}
+          disabled={ans !== null}
+          />
+        ))}
         </div>
       </div>
     </div>
